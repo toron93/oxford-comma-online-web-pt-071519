c8a26f521d197b2151d73e2d7c663501372be0f3
Toron Seigler <toronseigler9@gmail.com>
2019-08-05 18:33:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
-
-end
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
+end
